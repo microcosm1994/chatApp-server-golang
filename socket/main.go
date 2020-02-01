b8451c51f5797d1aea644088d2d9d46e310e4c41
dubo <microcosm1994@gmail.com>
2020-02-01 23:35:20 +0800

    添加了socket连接池

--- socket/main.go
+++ socket/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/googollee/go-socket.io"
 )
 
-func NewServer() {
+func init() {
 	server, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatal(err)
@@ -16,33 +16,12 @@ func NewServer() {
 		s.Emit("res", "ssssssss")
 		fmt.Println("Url:", s.URL())
 		fmt.Println("connected:", s.ID())
-		s.SetContext("1111")
 		return nil
-	})
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-	server.OnError("/", func(s socketio.Conn, e error) {
-		fmt.Println("meet error:", e)
-	})
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		fmt.Println("closed", reason)
 	})
 	go server.Serve()
 	defer server.Close()
-	http.Handle("/socket.io/", server)
-	http.ListenAndServe(":8000", nil)
-	log.Println("Serving at localhost:8000...")
+	http.Handle("/", server)
+	log.Fatal(http.ListenAndServe(":9090", nil))
+	log.Println("Serving at localhost:9090...")
 }
 
